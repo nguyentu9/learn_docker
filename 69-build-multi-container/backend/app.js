4a7959d7cda733d24c392ec2c89bbe8632f59dc1
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-05-20 08:40:03 +0700

    5. 71. Dockerizing the Node.js

--- 69-build-multi-container/backend/app.js
+++ 69-build-multi-container/backend/app.js
@@ -85,7 +85,7 @@ app.delete("/goals/:id", async (req, res) => {
 
 mongoose.connect(
   // `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
-  `mongodb://localhost:27017/course-goals?authSource=admin`,
+  `mongodb://host.docker.internal:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
